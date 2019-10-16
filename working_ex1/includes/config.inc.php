83d258c4dcc30c3dcbc257bd27a86a058eb83a85
Nick Ferrari <njferrari14@gmail.com>
2019-10-16 10:21:50 -0500

    Initial commit. Working on Chapter 3 example.

---
+++ working_ex1/includes/config.inc.php
@@ -0,0 +1,26 @@
+<?php
+if (!defined('LIVE')) DEFINE('LIVE', false);
+DEFINE'CONTACT_EMAIL', 'you@example.com');
+define ('BASE_URI', '/Users/nferrari/Applications/XAMPP/htdocs/working_ex1/');
+define ('BASE_URL', 'localhost/working_ex1/');
+define ('MYSQL', BASE_URI . 'mysql.inc.php');
+session_start();
+// define error handling
+function my_error_handler($e_number, $e_message, $e_file,
+ $e_line, $e_vars) {
+	$message = "An error occured in script '$e_file' on e_line
+	 $e_line:\n$e_message\n";
+	$message .= "<pre>" .print_r(debug_backtrace(), 1) . "</pre>\n";
+	if (!LIVE) {
+		echo '<div class="aler alert-danger">' . nl2br($message)
+		 . '</div>';
+	} else {
+		error_log($message, 1, CONTACT_EMAIL,
+		 'From:admin@example.com');
+		if ($e_number != E_NOTICE) {
+			echo '<div class="alert alert-danger">A system error
+			 occured. We apologize for the incnvenience.</div>';
+		}
+	} // end of live if/else
+} // end of my_error_handler() def
+set_error_handler('my_error_handler');
