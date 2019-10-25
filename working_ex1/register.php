97b55bfaa0a8357348a2b02aabf26aefc1ba3d12
Nick Ferrari <njferrari14@gmail.com>
2019-10-25 17:13:44 -0500

    began register.php

---
+++ working_ex1/register.php
@@ -0,0 +1,14 @@
+<?php
+require(‘./includes/config.inc.php’);
+require(MYSQL);
+$page_title = ‘Register’;
+include(‘./includes/header.html’);
+?>
+<h1>Register</h1>
+<p>Access to the site’s content is available to registered users
+➥at a cost of $10.00 (US) per year. Use the form below to begin
+➥the registration process. <strong>Note: All fields are required.
+➥</strong> After completing this form, you’ll be presented with
+➥the opportunity to securely pay for your yearly subscription via
+➥<a href=”http://www.paypal.com”>PayPal</a>.</p>
+<?php include(‘./includes/footer.html’); ?>
