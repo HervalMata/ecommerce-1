a35dff8d5580229ef2e80e9ac02fa6601376a51d
Nick Ferrari <njferrari14@gmail.com>
2019-10-28 10:48:08 -0500

    updated index.php to include login.inc.php when the login form has been submitted. Created login.inc.php

--- working_ex1/index.php
+++ working_ex1/index.php
@@ -1,6 +1,9 @@
 <?php
 require('./includes/config.inc.php');
 require(MYSQL);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	include('./inlcudes/login.inc.php');
+}
 include('./includes/header.html');
 ?>
 <h1>Welcome</h1>
