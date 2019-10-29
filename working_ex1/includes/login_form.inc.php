4bc17aabaa620952d89a2afc73c2a17a67740f64
Nick Ferrari <njferrari14@gmail.com>
2019-10-29 12:58:45 -0500

    updated login.inc.php, changed weird quoatation marks to normal ones

--- working_ex1/includes/login_form.inc.php
+++ working_ex1/includes/login_form.inc.php
@@ -1,15 +1,20 @@
-
-			<form>
-			  <fieldset>
-			    <legend>Login</legend>
-			    <div class="form-group">
-			      <label for="email">Email address</label>
-			      <input type="text" class="form-control" id="email" placeholder="Enter email">
-			    </div>
-			    <div class="form-group">
-			      <label for="pass">Password</label>
-			      <input type="password" class="form-control" id="pass" placeholder="Password">
-			    </div>
-			    <button type="submit" class="btn btn-default">Login</button>
-			  </fieldset>
-			</form>
+<?php
+if (!isset($login_errors)) $login_errors = array();
+require('./includes/form_functions.inc.php');
+?>
+<form action="index.php" method="post" accept-charset="utf-8">
+	<fieldset>
+		<legend>Login</legend>
+		<?php
+		if (array_key_exists('login', $login_errors)) {
+			echo '<div class="alert alert-danger">' . $login_errors['login']
+			. '</div>';
+		}
+		create_form_input('email', 'email', '', $login_errors,
+		array('placeholder'=>'Email address'));
+		create_form_input('pass', 'password', '', $login_errors,
+		array('placeholder'=>'Password'));
+		?>
+		<button type="submit" class="btn btn-default">Login &rarr;</button>
+	</fieldset>
+</form>
--- working_ex1/register.php
+++ working_ex1/register.php
@@ -93,25 +93,25 @@ require_once('./includes/form_functions.inc.php');
 	the registration process. <strong>Note: All fields are required.
 	</strong> After completing this form, you'll be presented with
 	the opportunity to securely pay for your yearly subscription via
-	<a href=”http://www.paypal.com”>PayPal</a>.
+	<a href="http://www.paypal.com">PayPal</a>.
 </p>
-<form action=”register.php” method=”post” accept-charset=”utf-8”>
+<form action="register.php" method="post" accept-charset="utf-8">
 <?php
 create_form_input('first_name', 'text', 'First Name', $reg_errors);
 create_form_input('last_name', 'text', 'Last Name', $reg_errors);
 create_form_input('username', 'text', 'Desired Username',
 $reg_errors);
-echo '<span class=”help-block”>Only letters and numbers are
+echo '<span class="help-block">Only letters and numbers are
 allowed.</span>';
 create_form_input('email', 'email', 'Email Address', $reg_errors);
 create_form_input('pass1', 'password', 'Password', $reg_errors);
-echo '<span class=”help-block”>Must be at least 6 characters long,
+echo '<span class="help-block">Must be at least 6 characters long,
 with at least one lowercase letter, one uppercase letter, and one
 number.</span>';
 create_form_input('pass2', 'password', 'Confirm Password',
 $reg_errors);
 ?>
-<input type=”submit” name=”submit_button” value=”Next &rarr;”
-id=”submit_button” class=”btn btn-default” />
+<input type="submit" name="submit_button" value="Next &rarr;"
+id="submit_button" class="btn btn-default" />
 </form>
 <?php include('./includes/footer.html'); ?>
