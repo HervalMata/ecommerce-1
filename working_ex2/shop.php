5962a58ba92f8592cbc9446d04bac3314db0fb7a
Nick Ferrari <njferrari14@gmail.com>
2019-11-12 09:21:20 -0600

    initial shop.php code

---
+++ working_ex2/shop.php
@@ -0,0 +1,12 @@
+<?php
+require('./includes/config.inc.php');
+if (isset($_GET['type']) && ($_GET['type'] === 'goodies')) {
+	$page_title = 'Our Goodies, by Category';
+	$type = 'goodies';
+} else {
+	$page_title = 'Our Coffee Products';
+	$type = 'coffee';
+}
+include('./includes/header.php');
+require(MYSQL);
+$r = mysqli_query($dbc, "CALL select_categories('$type')");
