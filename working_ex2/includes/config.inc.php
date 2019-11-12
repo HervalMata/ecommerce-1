f66f9ee56992b39750484cef33c7ab0815eddce4
Nick Ferrari <njferrari14@gmail.com>
2019-11-12 09:16:54 -0600

    fixed config

--- working_ex2/includes/config.inc.php
+++ working_ex2/includes/config.inc.php
@@ -11,7 +11,8 @@ if (strtoupper(substr(php_uname('s'), 0, 3)) == 'WIN') {
 } else {
 	define ('BASE_URI', '/Applications/XAMPP/xamppfiles/htdocs/ecommerce/working_ex2/includes/');
 }
-
+define('BOX_BEGIN', '<!-- box begin --><div class="box alt"><div class="left-top-corner"><div class="right-top-corner"><div class="border-top"></div></div></div><div class="border-left"><div class="border-right"><div class="inner">');
+define('BOX_END', '</div></div></div><div class="left-bot-corner"><div class="right-bot-corner"><div class="border-bot"></div></div></div></div><!-- box end -->');
 function my_error_handler($e_number, $e_message, $e_file, $e_line, $e_vars) {
 	
 	// build the error message:
