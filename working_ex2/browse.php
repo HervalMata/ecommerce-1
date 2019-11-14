44e917ab50256a8c3d2bfadca9466350285212cf
Nick Ferrari <njferrari14@gmail.com>
2019-11-14 08:46:45 -0600

    initial browse.php code

---
+++ working_ex2/browse.php
@@ -0,0 +1,32 @@
+<?php
+require('./includes/config.inc.php');
+$type = $sp_cat = $category = false;
+if (isset($_GET['type'], $_GET['category'], $_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT, array('min_range' => 1))) {
+	$category = $_GET['category'];
+	$sp_cat = $_GET['id'];
+	if ($_GET['type'] === 'goodies') {
+		$type = 'goodies';
+	} elseif ($_GET['type'] === 'coffee') {
+		$type = 'coffee';
+	}
+}
+if (!$type || !$sp_cat || !$category) {
+	$page_title = 'Error!';
+	include('./includes/header.php');
+	include('./views/error.html');
+	include('./includes/footer.html');
+	exit();
+}
+$page_title = ucfirst($type) . ' to Buy::' . $category;
+include('./inlcudes.header.php')
+require(MYSQL);
+$r = mysqli_query($dbc, "CALL select_products('$type', $sp_cat)");
+if (mysqli_num_rows($r) > 0) {
+	if ($type === 'goodies') {
+		include('./views/list_goodies.html');
+	} elseif ($type == 'coffee') {
+		include('./views/list_coffees.html');
+	}
+} else { // inlcude the "noproducts" page:
+	include('./views/noproducts.html');
+}
