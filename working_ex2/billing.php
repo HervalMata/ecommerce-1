1a9ce247b2aa158a6f7e8647a9b9511d560a3c05
Nick Ferrari <njferrari14@gmail.com>
2019-11-19 10:40:53 -0600

    billing.php

---
+++ working_ex2/billing.php
@@ -0,0 +1,26 @@
+<?php
+require('./includes/config.inc.php');
+session_start();
+$uid = session_id();
+if (!isset($_SESSION['customer_id'])) {
+	$location = 'https://' . BASE_URL . 'checlout.php';
+	header("Location: $location");
+	exit();
+}
+require(MYSQL);
+$billing_errors = array();
+$page_title = 'Coffee - Checkout - Your Billing Information';
+include('./includes/checkout_header.php');
+$r = mysqli_query($dbc, "CALL get_shopping_cart_contents('$uid')");
+if (mysqli_num_rows($r) > 0) {
+	if (isset($_SESSION['shipping_for_billing']) && ($_SERVER['REQUEST_METHOD'] !== 'POST')) {
+		$values = 'SESSION';
+	} else {
+		$values = 'POST';
+	}
+	include('./views/billing.php');
+} else { // empty cart
+	include('./views/emptycart.php');
+}
+include('./includes/footer.php');
+?>
